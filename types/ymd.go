f2b8781d27ed57a0b43d3def3f1b5651dacd250d
Takaki Mine <mine@ess-g.com>
2022-08-10 11:58:13 +0900

    Ymd.Add()の計算仕様変更：日の計算を最後に行う様にしました

--- types/ymd.go
+++ types/ymd.go
@@ -13,11 +13,11 @@ import (
 )
 
 type (
-	//Ymd yyyyMMdd形式で年月日を表す整数型
+	// Ymd yyyyMMdd形式で年月日を表す整数型
 	Ymd int
 )
 
-//ToYmd 年月日からYmd型に変換します
+// ToYmd 年月日からYmd型に変換します
 func ToYmd(y, m, d int) (ymd Ymd, err error) {
 	_, err = ValidateYmd(y, m, d)
 	if err == nil {
@@ -26,7 +26,7 @@ func ToYmd(y, m, d int) (ymd Ymd, err error) {
 	return
 }
 
-//ParseYmd Ymd型に変換します
+// ParseYmd Ymd型に変換します
 func ParseYmd(i interface{}) (ymd Ymd, err error) {
 	err = ymd.Scan(i)
 	if err == nil {
@@ -35,7 +35,7 @@ func ParseYmd(i interface{}) (ymd Ymd, err error) {
 	return
 }
 
-//ParseYmd2 Ymd型に変換します
+// ParseYmd2 Ymd型に変換します
 func ParseYmd2(i interface{}, err *error) (ymd Ymd) {
 	var e error
 	ymd, e = ParseYmd(i)
@@ -45,13 +45,13 @@ func ParseYmd2(i interface{}, err *error) (ymd Ymd) {
 	return
 }
 
-//YmdNow 現在の日付（ローカル）を取得します
+// YmdNow 現在の日付（ローカル）を取得します
 func YmdNow() Ymd {
 	t := time.Now()
 	return Ymd(t.Year()*10000 + int(t.Month())*100 + t.Day())
 }
 
-//String string型変換
+// String string型変換
 func (ymd Ymd) String() string {
 	if ymd == 0 {
 		return ""
@@ -59,7 +59,7 @@ func (ymd Ymd) String() string {
 	return fmt.Sprintf("%08d", ymd)
 }
 
-//CsvFormat CSV出力用のstring型変換
+// CsvFormat CSV出力用のstring型変換
 func (ymd Ymd) CsvFormat() string {
 	if ymd == 0 {
 		return ""
@@ -67,7 +67,7 @@ func (ymd Ymd) CsvFormat() string {
 	return strconv.Itoa(int(ymd))
 }
 
-//Scan 年月日を読み取ります
+// Scan 年月日を読み取ります
 func (ymd *Ymd) Scan(i interface{}) error {
 	if conv.IsEmpty(i) {
 		*ymd = 0
@@ -97,7 +97,7 @@ func (ymd *Ymd) Scan(i interface{}) error {
 	return errors.WithStack(ErrValidate)
 }
 
-//Value driver.Valuerインターフェイスの実装
+// Value driver.Valuerインターフェイスの実装
 func (ymd Ymd) Value() (driver.Value, error) {
 	if ymd == 0 {
 		return nil, nil
@@ -105,7 +105,7 @@ func (ymd Ymd) Value() (driver.Value, error) {
 	return int64(ymd), nil
 }
 
-//UnmarshalJSON json.Unmarshalerインターフェイスの実装
+// UnmarshalJSON json.Unmarshalerインターフェイスの実装
 func (ymd *Ymd) UnmarshalJSON(b []byte) (err error) {
 	var s interface{}
 	if err = json.Unmarshal(b, &s); err != nil {
@@ -120,18 +120,18 @@ func (ymd *Ymd) UnmarshalJSON(b []byte) (err error) {
 	return
 }
 
-//MarshalJSON json.Marshalerの実装
+// MarshalJSON json.Marshalerの実装
 func (ymd *Ymd) MarshalJSON() ([]byte, error) {
 	return json.Marshal(int(*ymd))
 }
 
-//MarshalLogObject zapcore.ObjectMarshalerの実装
+// MarshalLogObject zapcore.ObjectMarshalerの実装
 func (ymd *Ymd) MarshalLogObject(enc zapcore.ObjectEncoder) error {
 	enc.AddString("ymd", ymd.String())
 	return nil
 }
 
-//Validate 年月日が正しいか確認します
+// Validate 年月日が正しいか確認します
 func (ymd Ymd) Validate() (bool, error) {
 	if ymd == 0 {
 		return true, nil
@@ -140,7 +140,7 @@ func (ymd Ymd) Validate() (bool, error) {
 	return ValidateYmd(y, m, d)
 }
 
-//Part 年月日の要素を取得します
+// Part 年月日の要素を取得します
 func (ymd Ymd) Part() (y, m, d int) {
 	d = int(ymd) % 100
 	x := int(ymd) / 100
@@ -149,7 +149,7 @@ func (ymd Ymd) Part() (y, m, d int) {
 	return
 }
 
-//GoTime go言語のTime型に変換します
+// GoTime go言語のTime型に変換します
 func (ymd Ymd) GoTime() (tm time.Time) {
 	if ymd != 0 {
 		y, m, d := ymd.Part()
@@ -159,7 +159,7 @@ func (ymd Ymd) GoTime() (tm time.Time) {
 	return
 }
 
-//LastDay 最終日を取得します
+// LastDay 最終日を取得します
 func LastDay(y, m int) (d int) {
 	if m < 1 || m > 12 {
 		return
@@ -173,39 +173,35 @@ func LastDay(y, m int) (d int) {
 	return
 }
 
-//Add 年、月、日を加算します（減算はマイナス値を引数にセットします）
+// Add 年、月、日を加算します（減算はマイナス値を引数にセットします）
 func (ymd Ymd) Add(y, m, d int) Ymd {
 	if ymd == 0 {
 		return 0
 	}
 	year, month, day := ymd.Part()
 
-	//日から計算を行います
-	if d != 0 {
-		year, month, day = AdjustDay(year, month, day+d)
-	}
+	// 元の日付を正しい値に修正します
+	year, month, day = AdjustDay(year, month, day)
 
 	if m != 0 {
 		year, month = AdjustMonth(year, month+m)
+		// 日が月末を超える場合は月末をセットします
 		if maxDay := LastDay(year, month); day > maxDay {
-			if d == 0 { //日付の加減算が無い場合は最終日にします
-				day = maxDay
-			} else if month == 12 { //最終月の場合は翌年
-				year++
-				month = 1
-				day -= maxDay
-			} else { //翌月
-				month++
-				day -= maxDay
-			}
+			day = maxDay
 		}
 	}
 
 	year += y
+
+	//日の計算を行います
+	if d != 0 {
+		year, month, day = AdjustDay(year, month, day+d)
+	}
+
 	return Ymd(year*10000 + month*100 + day)
 }
 
-//SetDay 日を指定した値で置き換えます
+// SetDay 日を指定した値で置き換えます
 func (ymd Ymd) SetDay(d int) Ymd {
 	if ymd == 0 {
 		return 0
@@ -219,42 +215,42 @@ func (ymd Ymd) SetDay(d int) Ymd {
 	return Ymd(xy*10000 + xm*100 + d)
 }
 
-//Prev 前日の値を取得します
+// Prev 前日の値を取得します
 func (ymd Ymd) Prev() Ymd {
 	return ymd.Add(0, 0, -1)
 }
 
-//Next 一日後の値を取得します
+// Next 一日後の値を取得します
 func (ymd Ymd) Next() Ymd {
 	return ymd.Add(0, 0, 1)
 }
 
-//Year 年を取得します
+// Year 年を取得します
 func (ymd Ymd) Year() int {
 	return int(ymd) / 10000
 }
 
-//Month 月を取得します
+// Month 月を取得します
 func (ymd Ymd) Month() int {
 	return (int(ymd) / 100) % 100
 }
 
-//Day 日を取得します
+// Day 日を取得します
 func (ymd Ymd) Day() int {
 	return int(ymd) % 100
 }
 
-//YearMonth 年月を取得します
+// YearMonth 年月を取得します
 func (ymd Ymd) YearMonth() Ym {
 	return Ym(ymd / 100)
 }
 
-//MonthDay 月日を取得します
+// MonthDay 月日を取得します
 func (ymd Ymd) MonthDay() Md {
 	return Md(ymd % 10000)
 }
 
-//Between 二つの日付の間に入るか判定します
+// Between 二つの日付の間に入るか判定します
 func (ymd Ymd) Between(f, t Ymd) bool {
 	if ymd == 0 || f == 0 || t == 0 {
 		return false
@@ -262,7 +258,7 @@ func (ymd Ymd) Between(f, t Ymd) bool {
 	return f <= ymd && ymd <= t
 }
 
-//BetweenMonth 2つの月の間に該当するか判定します。m1 > m2の場合は年を跨ぐ範囲として扱います
+// BetweenMonth 2つの月の間に該当するか判定します。m1 > m2の場合は年を跨ぐ範囲として扱います
 func (ymd Ymd) BetweenMonth(m1, m2 int) bool {
 	if ymd == 0 {
 		return false
@@ -270,12 +266,12 @@ func (ymd Ymd) BetweenMonth(m1, m2 int) bool {
 	return BetweenMonth(ymd.Month(), m1, m2)
 }
 
-//BetweenMonthDay 2つの月日の間に該当するか判定します。md1 > md2の場合は年を跨ぐ範囲として扱います
+// BetweenMonthDay 2つの月日の間に該当するか判定します。md1 > md2の場合は年を跨ぐ範囲として扱います
 func (ymd Ymd) BetweenMonthDay(md1, md2 Md) bool {
 	return ymd.MonthDay().Between(md1, md2)
 }
 
-//Min 指定した日付と比較して小さい値を返します
+// Min 指定した日付と比較して小さい値を返します
 func (ymd Ymd) Min(o Ymd) Ymd {
 	if ymd == 0 {
 		return o
@@ -286,7 +282,7 @@ func (ymd Ymd) Min(o Ymd) Ymd {
 	return ymd
 }
 
-//Max 指定した日付と比較して大きい値を返します
+// Max 指定した日付と比較して大きい値を返します
 func (ymd Ymd) Max(o Ymd) Ymd {
 	if ymd == 0 {
 		return o
@@ -297,7 +293,7 @@ func (ymd Ymd) Max(o Ymd) Ymd {
 	return ymd
 }
 
-//Days グレゴリウス暦1年1月1日からの経過日数を取得します
+// Days グレゴリウス暦1年1月1日からの経過日数を取得します
 func (ymd Ymd) Days() int {
 	if ymd == 0 {
 		return 0
